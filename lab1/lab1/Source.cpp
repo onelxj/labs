879cd42dc77a84551543a39b510c6c91b05fd2f4
Zakhar Pilipuk <zpilipuk@gmail.com>
2021-02-02 17:18:25 +0300

    lab1 add find, remove

--- lab1/lab1/Source.cpp
+++ lab1/lab1/Source.cpp
@@ -1,7 +1,9 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
 
 class table
 {
@@ -12,7 +14,7 @@ public:
         {
             std::string temp;
             file >> temp;
-            insertId(0, data.size(), temp);
+            insertId(temp);
         }
 
         file.close();
@@ -31,73 +33,146 @@ public:
             std::cout << "\n";
         }
     }
+
+    int find(const std::string& str)
+    {
+        return find(0, data[hash(str)].size(), str);
+    }
+
+    void print(const std::string& str, int pos)
+    {
+        std::cout << "Hash: " << hash(str) << "\n";
+        std::cout << data[hash(str)][pos] << "\n";
+    }
+
+    void remove(const std::string& str, int pos)
+    {
+        data[hash(str)].erase(data[hash(str)].begin() + pos);
+        if (data[hash(str)].empty())
+        {
+            data.erase(data.find(hash(str)));
+        }
+    }
 private:
 
     // methods
+
+    int find(size_t f, size_t l, const std::string& str)
+    {
+        unsigned int h = hash(str);
+        int n = l - f;
+
+        if (str == data[h][0])
+        {
+            return 0;
+        }
+
+        if (str == data[h][(l + f + 1) / 2])
+        {
+            return (l + f + 1) / 2;
+        }
+
+        if (str < data[h][(l + f + 1) / 2])
+        {
+            return find(0, (l + f + 1) / 2, str);
+        }
+        else
+        {
+            return find((l + f) / 2, l, str);
+        }
+    }
 
     unsigned int hash(const std::string& str)
     {
         return std::toupper(str[0]) + std::toupper(str[1]) + std::toupper(str[2]);
     }
 
-    void insertId(size_t f, size_t l, const std::string& str)
+    void insertId(const std::string& str)
     {
         unsigned int h = hash(str);
-        int n = l - f;
+        std::pair<unsigned int, std::vector<std::string>> newPair;
+        newPair.second.push_back(str);
+        newPair.first = h;
 
-        if (n == 1 || n == 0)
+        if (data.find(h) == data.end())
         {
-            std::pair<unsigned int, std::vector<std::string>> newPair;
-            newPair.second.push_back(str);
-            newPair.first = h;
-
-            if (n == 0)
-            {
-                data.push_back(newPair);
-                return;
-            }
-            if (h == data[f].first)
-            {
-                data[f].second.push_back(str);
-                return;
-            }
-            if (h < data[f].first)
-            {                
-                data.insert(data.begin() + f, newPair);
-                return;
-            }
-            else
-            {
-                data.insert(data.begin() + f + 1, newPair);
-                return;
-            }
-        }
-
-        if (h == data[(l+f + 1) / 2].first)
-        {
-            data[(l+f + 1) / 2].second.push_back(str);
+            data.insert(newPair);
             return;
         }
-
-        if (h < data[(l+f + 1) / 2].first)
-        {
-            return insertId(0, (l+f + 1) / 2, str);
-        }
-        else
-        {
-            return insertId((l + f) / 2, l, str);
-        }
+        
+        (*data.find(h)).second.push_back(str);
+        std::sort((*data.find(h)).second.begin(), (*data.find(h)).second.end());
     }
 
     //variables
 
-    std::vector<std::pair<unsigned int, std::vector<std::string>>> data;
+    std::map<unsigned int, std::vector<std::string>> data;
 };
 
+int menu()
+{
+    system("pause");
+    system("cls");
+    int c;
+    std::cout << "1. print\n2. find\n3. remove";
+    std::cin >> c;
+    system("cls");
+    return c;
+}
 
 int main()
 {
     table obj;
-    obj.print();
+    int c = menu();
+    while (true)
+    {
+        switch (c)
+        {
+        case 1:
+        {
+            obj.print();
+            c = menu();
+            break;
+        }
+        case 2:
+        {
+            std::string str;
+            std::cout << "Enter string: ";
+            std::cin >> str;
+            int pos = obj.find(str);
+            if (pos != -1)
+            {
+                obj.print(str, pos);
+            }
+            else
+            {
+                std::cout << "No such id!\n";
+            }
+            c = menu();
+            break;
+        }
+        case 3:
+        {
+            std::string str;
+            std::cout << "Enter string: ";
+            std::cin >> str;
+            int pos = obj.find(str);
+            if (pos != -1)
+            {
+                obj.remove(str, pos);
+            }
+            else
+            {
+                std::cout << "No such id!\n";
+            }
+            c = menu();
+            break;
+        }
+        default:
+        {
+            return 0;
+        }
+        }
+    }
     return 0;
 }
